357656973275cc5354df0796bc8d1287b193b78b
GaizkaBucco <gaizkabucco@gmail.com>
2022-08-20 21:23:10 +0200

    added CartWidget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import NavBar from "./components/NavBar"
+import ItemListContainer from "./components/ItemListContainer.js"
 
 const App = () => {
 	return (
 		<>
 			<Hero />
 			<NavBar />
+			<ItemListContainer />
 		</>
 	)
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,5 @@
+const ItemListContainer = () => {
+	return <div className='flex justify-center'>En construccion...</div>
+}
+
+export default ItemListContainer
